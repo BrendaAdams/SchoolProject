1c9266646cc5fb4824b6c4716741a058e9c667d6
BrendaAdams <71841413+BrendaAdams@users.noreply.github.com>
2020-09-29 18:00:17 -0400

    Update Project 1.py

--- Project 1.py
+++ Project 1.py
@@ -1,20 +1,17 @@
 ##Determining if a patient has a low temp., normal temp., elevated temp., or fever and determine if the temperature is considered a fever##
-low = 0             ##Katherine Darrigrand##
+##Worked together on all lines##
+low = 0
 normal = 0
 elevated = 0
 fever = 0
 
-temp1 = str(input("Please enter temp 1 in degrees F:"))
-temp2 = str(input("Please enter temp 2 in degrees F:"))
+temp1 = (input("Please enter temp 1 in degrees F:"))
+temp2 = (input("Please enter temp 2 in degrees F:"))
 
 
 avg = (float(temp1)+float(temp2))/ float(2.0)
-print(avg)
-
 
 ##Take average of two temperatures for one patient##
-
-##Brenda Adams##
 
 
 if (avg <= float('97')):     
@@ -25,32 +22,20 @@ elif (avg <= float('99')):
     
 elif (avg <= float('100.3')):
     print('elevated temp')
-
+    
 else:
     print('fever')
     
-  
-if avg > float(100.4): fever = True
-else: fever = False;
-
-while( avg < 100.4):
-    temp1 = str(input("Please enter temp 1 in degrees F:"))
-    temp2 = str(input("Please enter temp 2 in degrees F:"))
-    print(avg);
-    break
-while( avg < 100.4):
-    avg = (float(temp1)+float(temp2))/ float(2.0)
-    print(avg)
-
-    
-##Taken from homework chapter 3##
-
-
-##Katherine Darrigrand##
-
 
 if avg <= float('99') and avg >= float('97.1'):
     print("Normal temperature, nothing wrong")
 else:
     print("Patient needs further testing")
 
+  
+if avg > float(100.4): fever = True
+else: fever = False
+
+while (fever):
+    print ("Patient has fever")
+    break
